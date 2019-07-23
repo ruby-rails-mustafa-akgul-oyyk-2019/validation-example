859879563d59a89839bc2c3cc975c5cbee2c772f
boratanrikulu <boratanrikulu@gmail.com>
2019-07-23 13:59:37 +0300

    Add validations to Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,10 @@
 class Lesson < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+            presence: true
+
+  validates :max_person,
+            numericality: { greater_than_or_equal_to: 10,
+                            less_than_or_equal_to: 40 }
 end
