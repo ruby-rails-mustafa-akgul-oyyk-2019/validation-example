373ecfc7a9d0a9eae6a984eee84af89692bbb4d3
boratanrikulu <boratanrikulu@gmail.com>
2019-07-23 13:56:57 +0300

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :email,
+            :first_name,
+            :last_name,
+            :password,
+            presence: true
+
+  validates :email,
+            uniqueness: true
+
+  validates :password,
+            confirmation: true
 end
