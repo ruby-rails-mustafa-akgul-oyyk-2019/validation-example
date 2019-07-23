6f8471de19e130be5bad2d4067b8889d8b20d144
boratanrikulu <boratanrikulu@gmail.com>
2019-07-23 14:13:05 +0300

    Add validations to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :bio,
+            length: { maximum: 250 }
+
+  validates :age,
+            numericality: { greater_than_or_equal_to: 18,
+                            less_than_or_equal_to: 99 }
+
+  # Enums
+  enum gender: { male: 0,
+                 female: 1 }
 end
